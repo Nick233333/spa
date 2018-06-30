914ea63a3ce2a91e269ebb07a6ee699e7e01e5a8
nick <nick_php@163.com>
2018-06-30 14:57:40 +0800

    vee-validate 验证库

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -12,6 +12,20 @@ window.Vue = require('vue');
 import VueRouter from 'vue-router';
 import router from './router';
 import App from './components/App';
+import VeeValidate from 'vee-validate';
+import zh_CN from 'vee-validate/dist/locale/zh_CN'
+import VueI18n from 'vue-i18n';
+Vue.use(VueI18n);
+const i18n = new VueI18n({
+    locale: 'zh_CN',
+});
+Vue.use(VeeValidate, {
+    i18n,
+    i18nRootKey: 'validation',
+    dictionary: {
+        zh_CN
+    }
+});
 
 Vue.use(VueRouter);
 Vue.component('app', App);
--- resources/assets/js/components/register/RegisterForm.vue
+++ resources/assets/js/components/register/RegisterForm.vue
@@ -1,32 +1,36 @@
 <template>
     <div class="panel-body">
         <form class="form-horizontal" @submit.prevent="register">
-            <div class="form-group">
+            <div class="form-group" :class="{'has-error': errors.has('name')}">
                 <label for="email" class="col-md-4 control-label">用户名</label>
                 <div class="col-md-6">
-                    <input v-model="name" id="name" type="text" class="form-control" name="name" value="" required>
+                    <input v-validate="'required|min:3'" v-model="name" id="name" type="text" class="form-control" name="name" value="" required>
+                    <span class="help-block" v-show="errors.has('name')">{{ errors.first('name') }}</span>
                 </div>
             </div>
 
-            <div class="form-group">
+            <div class="form-group" :class="{'has-error': errors.has('email')}">
                 <label for="email" class="col-md-4 control-label">邮箱</label>
                 <div class="col-md-6">
-                    <input v-model="email" id="email" type="email" class="form-control" name="email" value="" required>
+                    <input v-validate="'required|email'" v-model="email" id="email" type="email" class="form-control" name="email" value="" required>
+                    <span class="help-block" v-show="errors.has('email')">{{ errors.first('email') }}</span>
                 </div>
             </div>
 
-            <div class="form-group">
+            <div class="form-group" :class="{'has-error': errors.has('password')}">
                 <label for="password" class="col-md-4 control-label">密码</label>
 
                 <div class="col-md-6">
-                    <input v-model="password" id="password" type="password" class="form-control" name="password" required>
+                    <input v-validate="'required|min:6'" v-model="password" id="password" type="password" class="form-control" name="password" required>
+                    <span class="help-block" v-show="errors.has('password')">{{ errors.first('password') }}</span>
                 </div>
             </div>
 
-            <div class="form-group">
+            <div class="form-group" :class="{'has-error': errors.has('password_confirmation')}">
                 <label for="password-confirm" class="col-md-4 control-label">确认密码</label>
                 <div class="col-md-6">
-                    <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                    <input v-validate="'required|min:6'" id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                    <span class="help-block" v-show="errors.has('password_confirmation')">{{ errors.first('password_confirmation') }}</span>
                 </div>
             </div>
 
